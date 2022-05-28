2c5a6f7d71055c38320a9ace7bb7be7becc01214
Dysaca22 <dilanc@uninorte.edu.co>
2022-05-28 16:35:54 -0500

    Ruta de bienvenida y conexión a la base de datos

--- web/index.js
+++ web/index.js
@@ -2,28 +2,25 @@ const express = require("express");
 const mysql = require("mysql");
 const app = express();
 
-const connection = mysql.createPool({
-    connectionLimit: 10,
-    host: process.env.MYSQL_HOST || "localhost",
-    user: process.env.MYSQL_USER || "root",
-    password: process.env.MYSQL_PASSWORD || "password",
-    database: process.env.MYSQL_DATABASE || "test",
-});
+var connection;
 
 app.get("/", (req, res) => {
-    connection.query("SELECT * FROM Student", (err, rows) => {
-        if (err) {
-            res.json({
-                success: false,
-                err,
-            });
-        } else {
-            res.json({
-                success: true,
-                rows,
-            });
-        }
-    });
+    res.send('Proyecto docker, Node js y git de Dylan, Laura y Mariana');
+});
+
+app.get("/connectDB", (req, res) => {
+    try {
+        connection = mysql.createPool({
+            connectionLimit: 10,
+            host: process.env.MYSQL_HOST || "localhost",
+            user: process.env.MYSQL_USER || "root",
+            password: process.env.MYSQL_PASSWORD || "password",
+            database: process.env.MYSQL_DATABASE || "test",
+        });
+        res.send("ok");
+    } catch {
+        res.send("nok");
+    }
 });
 
 app.listen(5000, () => console.log("listining on port 5000"));
