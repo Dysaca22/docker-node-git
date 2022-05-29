668788e4c758e0753a4cb996f5f890ac41a7decb
Dysaca22 <dilanc@uninorte.edu.co>
2022-05-29 18:21:03 -0500

    Solucionando error base de datos v5

--- web/index.js
+++ web/index.js
@@ -19,124 +19,139 @@ const configDB = {
     database: process.env.MYSQL_DATABASE || "test",
 };
 
-const connection = mysql.createConnection(configDB);
-connection.connect(function(err) {
-    if (err) {
-        return console.error("error: " + err.message);
-    }
-    console.log("Conectado a la base de datos");
-});
+const pool = mysql.createPool(configDB);
+
 
 app.get("/", (req, res) => {
     res.send("Proyecto docker, Node js y git de Dylan, Laura y Mariana");
 });
 
 app.get("/connectDB", (req, res) => {
-    if (connection.state === "disconnected") {
-        res.send("nok");
-    } else {
-        res.send("ok");
-    }
+    pool.getConnection(function(err, connection) {
+
+        if (connection.state === "disconnected") {
+            res.send("nok");
+        } else {
+            res.send("ok");
+        }
+        connection.release();
+    });
 });
 
 app.post("/createUser", (req, res) => {
-    connection.query(
-        "SELECT * FROM usuario WHERE nombreDeUsuario = '" +
-        req.query.nombreDeUsuario +
-        "' AND clave = '" +
-        req.query.clave +
-        "' AND idEvento = '" +
-        req.query.idEvento +
-        "'",
-        (err, rows) => {
-            if (rows.length === 0) {
-                connection.query(
-                    "INSERT INTO usuario (nombreDeUsuario, clave, idEvento) VALUES ('" +
-                    req.query.nombreDeUsuario +
-                    "', '" +
-                    req.query.clave +
-                    "', " +
-                    req.query.idEvento +
-                    ")"
-                );
-                res.send("ok");
-            } else {
-                res.send("nok");
+    pool.getConnection(function(err, connection) {
+
+        connection.query(
+            "SELECT * FROM usuario WHERE nombreDeUsuario = '" +
+            req.query.nombreDeUsuario +
+            "' AND clave = '" +
+            req.query.clave +
+            "' AND idEvento = '" +
+            req.query.idEvento +
+            "'",
+            (err, rows) => {
+                if (rows.length === 0) {
+                    connection.query(
+                        "INSERT INTO usuario (nombreDeUsuario, clave, idEvento) VALUES ('" +
+                        req.query.nombreDeUsuario +
+                        "', '" +
+                        req.query.clave +
+                        "', " +
+                        req.query.idEvento +
+                        ")"
+                    );
+                    res.send("ok");
+                } else {
+                    res.send("nok");
+                }
             }
-        }
-    );
+        );
+        connection.release();
+    });
 });
 
 app.get("/searchUser", (req, res) => {
-    connection.query(
-        "SELECT idUsuario FROM usuario WHERE nombreDeUsuario = '" +
-        req.query.nombreDeUsuario +
-        "' AND clave = '" +
-        req.query.clave +
-        "' AND idEvento = '" +
-        req.query.idEvento +
-        "'",
-        (err, rows) => {
-            if (rows.length === 0) {
-                res.send("nok");
-            } else {
-                res.json(rows);
+    pool.getConnection(function(err, connection) {
+        connection.query(
+            "SELECT idUsuario FROM usuario WHERE nombreDeUsuario = '" +
+            req.query.nombreDeUsuario +
+            "' AND clave = '" +
+            req.query.clave +
+            "' AND idEvento = '" +
+            req.query.idEvento +
+            "'",
+            (err, rows) => {
+                if (rows.length === 0) {
+                    res.send("nok");
+                } else {
+                    res.json(rows);
+                }
             }
-        }
-    );
+        );
+        connection.release();
+    });
 });
 
 app.get("/deleteAll", (req, res) => {
-    connection.query("DELETE FROM usuario", (err, rows) => {
-        if (err) {
-            res.send("Error en la eliminación");
-        } else {
-            res.send("Se han eliminado los usuarios correctamente.");
-        }
+    pool.getConnection(function(err, connection) {
+        connection.query("DELETE FROM usuario", (err, rows) => {
+            if (err) {
+                res.send("Error en la eliminación");
+            } else {
+                res.send("Se han eliminado los usuarios correctamente.");
+            }
+        });
+        connection.release();
     });
 });
 
 app.post("/loadCSV", async(req, res) => {
-    if (!req.files) return res.status(400).send("Sin archivos cargados");
+    pool.getConnection(function(err, connection) {
+        if (!req.files) return res.status(400).send("Sin archivos cargados");
 
-    try {
-        const file = req.files.file;
+        try {
+            const file = req.files.file;
 
-        await csvtojson()
-            .fromFile(file.tempFilePath)
-            .then(async(source) => {
-                try {
-                    for (var i = 0; i < source.length; i++) {
-                        var nombreDeUsuario = source[i]["nombreDeUsuario"],
-                            clave = source[i]["clave"],
-                            idEvento = source[i]["idEvento"];
+            await csvtojson()
+                .fromFile(file.tempFilePath)
+                .then(async(source) => {
+                    try {
+                        for (var i = 0; i < source.length; i++) {
+                            var nombreDeUsuario = source[i]["nombreDeUsuario"],
+                                clave = source[i]["clave"],
+                                idEvento = source[i]["idEvento"];
 
-                        await connection.query(
-                            `INSERT INTO usuario (nombreDeUsuario, clave, idEvento) values(${nombreDeUsuario}, ${clave}, ${idEvento})`,
-                            (err, rows) => {
-                                if (err) {
-                                    res.status(400).send(`Error ingresando la fila ${i + 1}`);
+                            await connection.query(
+                                `INSERT INTO usuario (nombreDeUsuario, clave, idEvento) values(${nombreDeUsuario}, ${clave}, ${idEvento})`,
+                                (err, rows) => {
+                                    if (err) {
+                                        res.status(400).send(`Error ingresando la fila ${i + 1}`);
+                                    }
                                 }
-                            }
-                        );
+                            );
+                        }
+                        res.send("Se agregaron todos los elementos correctamente");
+                    } catch {
+                        res.send("Hubo un error cargando el csv");
                     }
-                    res.send("Se agregaron todos los elementos correctamente");
-                } catch {
-                    res.send("Hubo un error cargando el csv");
-                }
-            });
-    } catch {
-        res.send("El nombre del parametro del archivo debe llamarse 'file'");
-    }
+                });
+        } catch {
+            res.send("El nombre del parametro del archivo debe llamarse 'file'");
+        }
+        connection.release();
+    });
 });
 
 app.get("/read", (req, res) => {
-    connection.query("SELECT * FROM usuario", (err, rows) => {
-        if (err) {
-            res.send(err);
-        } else {
-            res.json(rows);
-        }
+    pool.getConnection(function(err, connection) {
+        connection.query("SELECT * FROM usuario", (err, rows) => {
+            if (err) {
+                res.send(err);
+            } else {
+                res.json(rows);
+            }
+        });
+        connection.release();
     });
 });
 
