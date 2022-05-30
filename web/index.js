607e74572b61aa7f36ff888505e466af7bb0158b
Dysaca22 <dilanc@uninorte.edu.co>
2022-05-30 13:20:50 -0500

    Arreglando método loadCSV v17

--- web/index.js
+++ web/index.js
@@ -104,33 +104,34 @@ app.get("/deleteAll", (req, res) => {
     });
 });
 
-app.post("/loadCSV", (req, res) => {
+app.post("/loadCSV", async(req, res) => {
     if (!req.files) return res.send("Sin archivos cargados");
 
-    const conn = pool.getConnection();
-    try {
-        const file = req.files.file;
-        csvtojson()
-            .fromFile(file.tempFilePath)
-            .then((source) => {
-                try {
-                    for (var i = 0; i < source.length; i++) {
-                        var nombreDeUsuario = source[i]["nombreDeUsuario"],
-                            clave = source[i]["clave"],
-                            idEvento = source[i]["idEvento"];
-                        console.log(i);
-                        agregarUsuario(conn, nombreDeUsuario, clave, idEvento);
+    pool.getConnection((err, connection) => {
+
+        try {
+            const file = req.files.file;
+            csvtojson()
+                .fromFile(file.tempFilePath)
+                .then((source) => {
+                    try {
+                        for (var i = 0; i < source.length; i++) {
+                            var nombreDeUsuario = source[i]["nombreDeUsuario"],
+                                clave = source[i]["clave"],
+                                idEvento = source[i]["idEvento"];
+                            agregarUsuario(connection, nombreDeUsuario, clave, idEvento);
+                        }
+                        res.send("ok");
+                    } catch (e) {
+                        console.log(e);
+                        res.send("Hubo un error cargando el csv");
                     }
-                    res.send("ok");
-                } catch (e) {
-                    console.log(e);
-                    res.send("Hubo un error cargando el csv");
-                }
-            });
-    } catch {
-        res.send("El nombre del parametro del archivo debe llamarse 'file'");
-    }
-    conn.end();
+                });
+        } catch {
+            res.send("El nombre del parametro del archivo debe llamarse 'file'");
+        }
+        connection.end();
+    });
 });
 
 function agregarUsuario(conn, nombreDeUsuario, clave, idEvento) {
